6f60e5fecb443589087dbbc9e4ba5c8138812374
smaretick <scottmaretick51@gmail.com>
2022-05-17 23:21:29 -0500

    Add files via upload

---
+++ STAKE/BeauSoup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+import unittest, time, re, os
+import timeit
+import shutil
+import json
+import smtplib, ssl #needed for sending email
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from datetime import datetime
+from datetime import date
+from datetime import datetime
+
+filename = datetime.now().strftime("%Y%m%d")
+print(filename)
+extension = ".txt"
+file = filename + extension
+f = open(file, 'w')
+
+url = 'https://cryptomaniaks.com/stake-review/'
+res = requests.get(url)
+html_page = res.content
+soup = BeautifulSoup(html_page, 'html.parser')
+text = soup.find_all(text=True)
+
+output = ''
+blacklist = [
+    '[document]',
+    'noscript',
+    'header',
+    'html',
+    'meta',
+    'head', 
+    'input',
+    'script',
+    # there may be more elements you don't want, such as "style", etc.
+]
+
+for t in text:
+    if t.parent.name not in blacklist:
+        output += '{} '.format(t)
+
+#print(output)
+f.write(output)
+f.close()
---
+++ STAKE/README-STAKE-DIFF2.txt
@@ -0,0 +1,17 @@
+DIFF APPS
+=========
+1. html2text
+	html2text is a Python script that converts a page of HTML into clean, easy-to-read plain ASCII text. Better yet
+2. brew install html2text
+3. npm install html-to-text
+4. BeautifulSoup4 (pip3 install beautifulsoup4)
+=====================================
+python3 BeauSoup.py > beautiful_soup-09292021.txt
+4. python3 BeauSoup.py > beautiful_soup-MMDDYYYY.txt
+5. diff beautiful_soup-MMDDYYYY.txt(0) beautiful_soup-MMDDYYYY.txt(1)
+=====================================
+=====================================
+=====================================
+A. pip3 install requests beautifulsoup4 
+B. Add url of site you want to analyze to /Users/scott/Desktop/Beautiful-Soup/BS-PROD/BeauSoup.py
+C. python3 BeauSoup.py (stdout) or python3 BeauSoup.py>BeautifulSoup.txt
